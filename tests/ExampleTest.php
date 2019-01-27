328aaa5afb8a6e5172416ddcab33b580dc4946df
mitsuru793 <mitsuru793@gmail.com>
2019-01-27 19:31:29 +0900

    setup with boilerplate

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Php;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testHello()
+    {
+        $this->assertTrue(true);
+    }
+}
